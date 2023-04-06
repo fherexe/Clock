47ce8cd0691c5f8d7c0d791581640d45e9f4bf63
Fernando Souza <114751334+fherexe@users.noreply.github.com>
2023-04-06 01:10:39 -0300

    att

--- script.js
+++ script.js
@@ -4,11 +4,7 @@ function clock () {
     const horas = horaAgora.getHours ();
     const minutos = horaAgora.getMinutes ();
     const segundos = horaAgora.getSeconds ();
-   //const formatoHoras = horas.toString ()
-    //.padStart (2, "0");
-
-    const formatoHoras = 19
-
+    const formatoHoras = horas.toString ().padStart (2, "0"); //const formatoHors = 12
     const formatoMinutos = minutos.toString ().padStart (2, "0");
     const formatoSegundos = segundos.toString ().padStart (2, "0");
          //.padStart ("0"); definir que o valor vai começar com 0
@@ -32,23 +28,23 @@ function clock () {
     ` 
     //Aspas `` libera o uso de placeholders ${}
 
-    if (6 <=  formatoHoras){ 
+    if (6 <=  parseInt(formatoHoras, 10) <19){ 
         document.querySelector("main").setAttribute("class","morning-box");
         document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')";
     }
 
-    if (12 <= formatoHoras){
+    if (12 <= parseInt(formatoHoras, 10) <19){
         document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1592281175375-a73f0a55127f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1504&q=80')";
         document.querySelector("main").setAttribute("class","afternoon-box");
     }
 
-    if (19 <= formatoHoras){
+    if (19 <= parseInt(formatoHoras, 10) <6){
         document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1506606401543-2e73709cebb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')";
         document.querySelector("main").setAttribute("class","night-box");
     }
-
 }
 
 setInterval(clock, 1000);
 
 
+//Operadores lógicos não  estão funcionando pois o dado que estão tentando ler é de uma string
